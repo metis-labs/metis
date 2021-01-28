c8e888dd8a3d3566a2bf0e42c58090c395571466
Youngteac Hong <youngteac.hong@navercorp.com>
2021-01-28 17:53:49 +0900

    Add code syntax highlighting

--- src/components/CodeView/index.tsx
+++ src/components/CodeView/index.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
       marginTop: '64px',
+      paddingTop: '30px',
       height: 'calc(100vh - 64px)',
-      backgroundColor: '#eaeaeb',
+      backgroundColor: '#f8f8ff',
     },
   }),
 );
 
+const codeString = `import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+`;
+
 export default function CodeView() {
   const classes = useStyles();
-  return <div className={classes.root}>CodeView</div>;
+
+  return (
+    <div className={classes.root}>
+      <SyntaxHighlighter language="python" style={docco} showLineNumbers>
+        {codeString}
+      </SyntaxHighlighter>
+    </div>
+  );
 }
